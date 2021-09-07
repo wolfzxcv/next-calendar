ed064fd104d6c4dcb9fbb53b47020107890c839d
Alice Chou <sincereitsl.it3@gmail.com>
2021-09-07 17:52:09 +0800

    modify settings

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyCk6jDCbSX-Ioj47TjtCvlQh48LKPf0Vb0',
+  authDomain: 'report-system-385be.firebaseapp.com',
+  projectId: 'report-system-385be',
+  storageBucket: 'report-system-385be.appspot.com',
+  messagingSenderId: '862977532012',
+  appId: '1:862977532012:web:5bddc5b3604d1f80453849',
+  measurementId: 'G-K27YYPC209'
+};
--- src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './openChatWindow';
-export * from './scrollToTop';
